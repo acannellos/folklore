9ee6e32d19e759e70a22d6f88285bea01b22589e
alcann <alex.cannellos@gmail.com>
2024-06-19 15:11:33 -0400

    refactor actions

--- scripts/global/global.gd
+++ scripts/global/global.gd
@@ -1,5 +1,5 @@
 extends Node
 
-const player_data = preload("res://data/entities/entity_data_player.tres")
+#const player_data = preload("res://data/entities/entity_data_player.tres")
 #var player_grid_pos: Vector2i = Vector2i.ZERO
 #var player_steps: int
